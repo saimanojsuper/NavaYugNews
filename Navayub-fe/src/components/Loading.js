0837b5caff6eb405361ee580d073ba68293fd0f6
Saimanoj Napa <saimanoj.napa@appian.com>
2024-10-06 16:11:54 +0530

    adding forget changes to commit for loading

---
+++ Navayub-fe/src/components/Loading.js
@@ -0,0 +1,12 @@
+import "./loading.css";
+
+const Loading = ()=> {
+    return(
+        <div className="loading-container">
+        <div className="loading-spinner"></div>
+        <p className="loading-text">Loading...</p>   
+  
+      </div>
+    )
+}
+export default Loading;
